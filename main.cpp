6d166d33e41e97bf81ec19d353890c18a2a9affa
intermet <mustapha.ziy@gmail.com>
2018-05-08 21:48:29 +0200

    modifié :         main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include "relation.hpp"
-int main(){
-  Relation<int, 2> r1 = Relation<int, 2>("facebook.dat");
-  Relation<int, 3> res = Relation<int, 3>();
+std::
+int main(int argc, char** argv){
+  Relation<int, 2> r1 = Relation<int, 2>(argv[1]);
+  Relation<int, 3> r2 = Relation<int, 3>();
+  Relation<int, 3> r3 = Relation<int, 3>();
+
   vector<string> var1 {"x", "y"};
   vector<string> var2 {"y", "z"};
+  vector<string> var3 {"x", "z"};
+  vector<string> var  {"x", "y", "z"};
 
-  join<int, 2, 2, 3, 1>(r1, var1, r1, var2, res);
-  res.to_file("res");
+  join<int, 2, 2, 3, 1>(r1, var1, r1, var2, r2);
+  join<int, 3, 2, 3, 2>(r2, var, r1, var3, r3);
+  r3.to_file("res");
 }
