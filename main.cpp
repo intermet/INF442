d9331cf720549b3296a0f00efef76053636dba05
intermet <mustapha.ziy@gmail.com>
2018-05-27 01:49:48 +0200

    report added

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 int main(int argc, char** argv){
   MPI::Init(argc, argv);
   int rank = MPI::COMM_WORLD.Get_rank();
-  vector<string> vars1 {"x", "y"};
+  vector<string> vars1 {"x", "x", "y"};
   vector<string> vars2 {"y", "z"};
   Relation r1 = Relation(vars1);
   Relation r2 = Relation(vars2);
   if (rank == 0){
     vector<string> v = join_vars(vars1, vars2);
-    r1 = Relation(argv[1], 2);
+    r1 = Relation(argv[1], 3);
     r2 = Relation(argv[2], 2);
     r1.to_file("r1");
     r2.to_file("r2");
